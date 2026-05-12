174ac8705b853272906c97c851f2b5fc1334e17f
Chegini <amirchegini007@gmail.com>
2026-05-12 18:11:18 +0330

    feat: add initial call request shared types

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -1,1 +1,16 @@
-export * from './lib/shared-types.js';
+export enum CallRequestStatus {
+  REQUESTED = 'REQUESTED',
+}
+
+export interface CreateCallRequestDto {
+  email: string;
+  phoneNumber: string;
+  scheduledAt: string;
+}
+
+export interface CallRequestedEvent {
+  callRequestId: string;
+  email: string;
+  phoneNumber: string;
+  scheduledAt: string;
+}
